97b19f0de3c0e1c30f31e285d779f18437c8f291
somarusomayama <somayamasomaru@gmail.com>
2025-12-10 22:56:31 +0000

    add env test

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,4 +1,7 @@
 import { test, expect } from '@playwright/test';
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 test('has title', async ({ page }) => {
   await page.goto('/');
@@ -15,4 +18,12 @@ test('get started link', async ({ page }) => {
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+
+test('read env', async ({ page }) => {
+  await page.goto('/');
+  await page.getByRole('button', { name: 'Search (Ctrl+K)' }).click();
+  await page.getByRole('searchbox', { name: 'Search' }).fill(process.env.KEYWORD ?? 'test');
+  await page.getByRole('searchbox', { name: 'Search' }).press('Enter');
+  await expect(page.getByRole('button', { name: 'Guides' })).toBeVisible();
 });
